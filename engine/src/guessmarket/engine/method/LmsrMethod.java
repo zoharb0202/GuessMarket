2a6f92cd23d804aa730ae2081e5bd1946c105710
zoharb0202 <zoharb0202@gmail.com>
2026-08-04 21:07:56 +0300

    Update LmsrMethod.java

--- engine/src/guessmarket/engine/method/LmsrMethod.java
+++ engine/src/guessmarket/engine/method/LmsrMethod.java
@@ -9,24 +9,28 @@ public class LmsrMethod extends TradingMethod {
 
     private final int b;
 
-    public LmsrMethod(List<EventOption> options, int b) {
+    public LmsrMethod(List<EventOption> options, int b)
+    {
         super(options);
         this.b = b;
     }
 
-    public int getB() {
+    public int getB() 
+    {
         return b;
     }
 
     @Override
-    public double getOptionValue(int optionIndex) {
+    public double getOptionValue(int optionIndex)
+    {
         int[] quantities = currentQuantities();
         int max = findMax(quantities);
         return weightOf(quantities[optionIndex], max) / sumOfWeights(quantities, max);
     }
 
     @Override
-    public double getBuyCost(int optionIndex, int quantity) {
+    public double getBuyCost(int optionIndex, int quantity)
+    {
         int[] before = currentQuantities();
         int[] after = currentQuantities();
         after[optionIndex] += quantity;
@@ -34,11 +38,13 @@ public class LmsrMethod extends TradingMethod {
     }
 
     @Override
-    public double getInitialSubsidy() {
+    public double getInitialSubsidy() 
+    {
         return costOf(new int[getOptions().size()]);
     }
 
-    private int[] currentQuantities() {
+    private int[] currentQuantities() 
+    {
         List<EventOption> options = getOptions();
         int[] quantities = new int[options.size()];
         for (int i = 0; i < options.size(); i++) {
@@ -47,32 +53,36 @@ public class LmsrMethod extends TradingMethod {
         return quantities;
     }
 
-    // C(q) = b * ln( sum of e^(qi/b) ).
-    // the biggest quantity is pulled out of the exponent, otherwise a large purchase
-    // makes Math.exp return infinity and every number after that becomes NaN
-    private double costOf(int[] quantities) {
+    private double costOf(int[] quantities) 
+    {
         int max = findMax(quantities);
         return max + b * Math.log(sumOfWeights(quantities, max));
     }
 
-    private double sumOfWeights(int[] quantities, int max) {
+    private double sumOfWeights(int[] quantities, int max)
+    {
         double sum = 0;
-        for (int quantity : quantities) {
+        for (int quantity : quantities) 
+        {
             sum += weightOf(quantity, max);
         }
         return sum;
     }
 
-    private double weightOf(int quantity, int max) {
+    private double weightOf(int quantity, int max) 
+    {
         return Math.exp((quantity - max) / (double) b);
     }
 
-    private int findMax(int[] quantities) {
+    private int findMax(int[] quantities)
+    {
         int max = quantities[0];
-        for (int quantity : quantities) {
-            if (quantity > max) {
-                max = quantity;
-            }
+        for (int quantity : quantities)
+            {
+                if (quantity > max) 
+                {
+                    max = quantity;
+                }
         }
         return max;
     }
