37bccbf4afbd29f3391ec081616c13af4de2e6b7
zoharb0202 <zoharb0202@gmail.com>
2026-08-04 21:09:36 +0300

    Update CommissionType.java

--- engine/src/guessmarket/engine/model/CommissionType.java
+++ engine/src/guessmarket/engine/model/CommissionType.java
@@ -1,24 +1,30 @@
 package guessmarket.engine.model;
 
-public enum CommissionType {
+public enum CommissionType
+    {
     ON_PURCHASE("on-purchase", "on every purchase"),
     ON_CLOSE("on-close", "when the event is closed");
 
     private final String fileValue;
     private final String description;
 
-    CommissionType(String fileValue, String description) {
+    CommissionType(String fileValue, String description) 
+    {
         this.fileValue = fileValue;
         this.description = description;
     }
 
-    public String getDescription() {
+    public String getDescription() 
+    {
         return description;
     }
 
-    public static CommissionType fromFileValue(String value) {
-        for (CommissionType type : values()) {
-            if (type.fileValue.equalsIgnoreCase(value)) {
+    public static CommissionType fromFileValue(String value)
+    {
+        for (CommissionType type : values()) 
+        {
+            if (type.fileValue.equalsIgnoreCase(value)) 
+            {
                 return type;
             }
         }
