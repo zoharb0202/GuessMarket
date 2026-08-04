5b2444b13dbda6d9aaae5fd876680b39f4b0c056
zoharb0202 <zoharb0202@gmail.com>
2026-08-04 21:05:21 +0300

    Update PurchaseResultDto.java

--- engine/src/guessmarket/engine/dto/PurchaseResultDto.java
+++ engine/src/guessmarket/engine/dto/PurchaseResultDto.java
@@ -1,23 +1,28 @@
 package guessmarket.engine.dto;
 
-public class PurchaseResultDto {
-    private final double sharesCost;
-    private final double commission;
+public class PurchaseResultDto
+    {
+        private final double sharesCost;
+        private final double commission;
 
-    public PurchaseResultDto(double sharesCost, double commission) {
+    public PurchaseResultDto(double sharesCost, double commission)
+    {
         this.sharesCost = sharesCost;
         this.commission = commission;
     }
 
-    public double getSharesCost() {
+    public double getSharesCost() 
+    {
         return sharesCost;
     }
 
-    public double getCommission() {
+    public double getCommission() 
+    {
         return commission;
     }
 
-    public double getTotalPaid() {
+    public double getTotalPaid()
+    {
         return sharesCost + commission;
     }
 }
