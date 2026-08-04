1f0d13d86d697984ffa3569a7860b6387f44a99d
zoharb0202 <zoharb0202@gmail.com>
2026-08-04 21:15:02 +0300

    Update EventStatus.java

--- engine/src/guessmarket/engine/model/EventStatus.java
+++ engine/src/guessmarket/engine/model/EventStatus.java
@@ -1,16 +1,19 @@
 package guessmarket.engine.model;
 
-public enum EventStatus {
+public enum EventStatus 
+{
     ACTIVE("Active"),
     CLOSED("Closed");
 
     private final String description;
 
-    EventStatus(String description) {
+    EventStatus(String description) 
+    {
         this.description = description;
     }
 
-    public String getDescription() {
+    public String getDescription() 
+    {
         return description;
     }
 }
