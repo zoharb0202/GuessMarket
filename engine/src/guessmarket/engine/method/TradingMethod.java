16d9f2aedb72dff9d37d147c7164651eb515231a
zoharb0202 <zoharb0202@gmail.com>
2026-08-04 21:08:21 +0300

    Update TradingMethod.java

--- engine/src/guessmarket/engine/method/TradingMethod.java
+++ engine/src/guessmarket/engine/method/TradingMethod.java
@@ -5,20 +5,18 @@ import guessmarket.engine.model.EventOption;
 import java.io.Serializable;
 import java.util.List;
 
-/**
- * A trading method decides how much an option is worth and how much a purchase costs.
- * For now the only method is LMSR, but the file format already allows other methods.
- */
 public abstract class TradingMethod implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private final List<EventOption> options;
 
-    protected TradingMethod(List<EventOption> options) {
+    protected TradingMethod(List<EventOption> options)
+    {
         this.options = options;
     }
 
-    protected List<EventOption> getOptions() {
+    protected List<EventOption> getOptions() 
+    {
         return options;
     }
 
